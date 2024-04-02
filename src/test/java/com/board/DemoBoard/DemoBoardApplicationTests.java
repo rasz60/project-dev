f0494473b9761f48449db199206b4bbdb781a90b
jinwoong <rassayzsixt@gmail.com>
2024-04-02 11:00:19 +0900

    first commit

---
+++ src/test/java/com/board/DemoBoard/DemoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.DemoBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
